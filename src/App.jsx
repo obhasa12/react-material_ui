4a326d78eb4df6ad0c055a1daadc470cdca8cef7
Ninja <cuansenputra@gmail.com>
2023-06-28 17:15:55 +0700

    material ui form demo

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import RatingDemo from './RatingDemo';
+import FormDemo from './FormDemo';
+
+function App() {
+  
+  return (
+    <div>
+      {/* <Button variant="text" color="success">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Button 
+        size="large" 
+        variant="contained" 
+        color="error"
+        onClick={() => alert("hi")}
+      >
+        Contained
+      </Button>
+      <IconButton color="secondary" aria-label="add an alarm">
+        <AlarmIcon />
+      </IconButton> */}
+      {/* <RatingDemo /> */}
+      <FormDemo />
+    </div>
+  )
+}
+
+export default App
---
+++ src/FormDemo.jsx
@@ -0,0 +1,10 @@
+import { TextField } from "@mui/material";
+function FormDemo() {
+    return ( 
+        <div>
+            <TextField id="outlined-basic" label="Puppy name" variant="outlined" placeholder="puppy name"/>
+        </div>
+     );
+}
+
+export default FormDemo;
